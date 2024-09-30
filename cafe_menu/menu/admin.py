a29544f8e88b5f46c503296072d154184a12bdcd
aruytehno <aruytehno@yandex.ru>
2024-09-30 18:32:10 +0300

    Добавил отображение товара в меню

--- cafe_menu/menu/admin.py
+++ cafe_menu/menu/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 from .models import Product
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('item_name', 'item_price', 'item_in_stock', 'display_image')
+    fields = ('item_name', 'item_category', 'item_ingredients', 'item_mass', 'item_price', 'item_in_stock', 'item_image', 'display_image')
+    readonly_fields = ('display_image',)  # Чтобы сделать поле только для чтения
 
-# Register your models here.
+    def display_image(self, obj):
+        if obj.item_image:
+            return format_html('<img src="{}" style="width: 100px; height: auto;" />', obj.item_image.url)
+        return "Нет изображения"
+    display_image.short_description = 'Изображение'  # Заголовок для поля
 
+    list_filter = ('item_category',)
+    search_fields = ('item_name',)
 
-@admin.register(Product)
-class EcoProductAdmin(admin.ModelAdmin):
-    # list display
-    list_display = ['item_name', 'item_price', 'item_category', 'item_in_stock']
-    # list Filter
-    list_filter = ['item_category', 'item_in_stock']
-    # search list
-    search_fields = ['item_name']
+admin.site.register(Product, ProductAdmin)
--- cafe_menu/menu/models.py
+++ cafe_menu/menu/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Product(models.Model):
     item_category = models.CharField(max_length=50, verbose_name='Категория')
     item_name = models.CharField(max_length=100, verbose_name='Наименование')
-    item_ingredients = models.TextField(max_length=500, verbose_name='Ингридиенты', blank=True)
+    item_ingredients = models.TextField(max_length=500, verbose_name='Ингредиенты', blank=True)
     item_mass = models.CharField(max_length=50, verbose_name='Масса/объём', blank=True)
     item_price = models.IntegerField(verbose_name='Цена')
     item_in_stock = models.BooleanField(verbose_name='В наличии')
-    item_image = models.ImageField(upload_to='products/', verbose_name='Изображение товара', blank=True, null=True)
+    item_image = models.ImageField(upload_to='products/', verbose_name='Изображение', blank=True, null=True)  # Добавьте это поле
 
     class Meta:
         verbose_name = 'товар'
